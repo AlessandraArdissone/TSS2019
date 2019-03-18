f5fb0534af263edd6fe450f0c18d971c4fa7fad9
AlessandraArdissone <perlatempesta77@gmail.com>
2019-03-18 13:29:00 +0100

    calcolatrice keyboard event

--- JavaScript/Lez11_Marchisio_gridLayout/public_html/js/gridLayout.js
+++ JavaScript/Lez11_Marchisio_gridLayout/public_html/js/gridLayout.js
@@ -6,4 +6,37 @@
 document.querySelectorAll(".numero").forEach(function (elm, k) {
         elm.setAttribute("num", k);
         elm.innerHTML = k;
+        elm.onclick = scriviNumero;
     });
+
+function scriviNumero() {
+    scrivi(this.innerHTML);
+}
+
+function scrivi(str) {
+    document.querySelector("#schermo").innerHTML += str;
+}
+
+document.querySelector(".virgola").onclick = function() {document.querySelector("#schermo").innerHTML += ",";};
+
+document.querySelector("body").onkeydown = function (e) {
+    console.log("-------------------");
+    console.log(e.which);
+    console.log(e.key);
+//  controllaAereo(e.which, e.shiftKey, e.ctrlKey);
+    switch (e.key) {
+        case "0":
+        case "1":
+        case "2":
+        case "3":
+        case "4":
+        case "5":
+        case "6":
+        case "7":
+        case "8":
+        case "9":
+            scrivi(e.key);
+            break;
+    }
+    console.log(e);
+};
