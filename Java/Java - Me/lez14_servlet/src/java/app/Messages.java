5b3cd338fa09398afd85894de900a81e1da3d610
AlessandraArdissone <perlatempesta77@gmail.com>
2019-03-19 17:05:54 +0100

    JSON con Marchisio

--- Java/Java - Me/lez14_servlet/src/java/app/Messages.java
+++ Java/Java - Me/lez14_servlet/src/java/app/Messages.java
@@ -7,6 +7,10 @@ package app;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Scanner;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,7 +21,7 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author tss
  */
-@WebServlet("/hello")
+@WebServlet("/datiJava")
 public class Messages extends HttpServlet {
 
     @Override
@@ -25,8 +29,36 @@ public class Messages extends HttpServlet {
         // http://localhost:8080/lez14_servlet/hello?id=1
         System.out.println("Ho ricevuto una richiesta.");
         System.out.println(req.getParameter("id"));
-        
+
+        try(Connection cn = DbManager.openConnection()) {
+
+            System.out.println("Inserisci le iniziali del cliente da cercare:");
+            Scanner s = new Scanner(System.in);
+
+            String ricerca = s.nextLine();
+
+            try (ResultSet rs = DbManager.searchCustomersByName(cn, ricerca)) {
+                System.out.println("Clienti trovati:");
+                while (rs.next()) {
+                    // CICLO SUI RECORD RESTITUITI DALLA QUERY
+                    String name = rs.getString("customerName");
+                    System.out.println(name);
+                }
+            }
+        } catch (SQLException ex) {
+            System.out.println("Esecuzione della query fallita :-( " + ex.getMessage() + " " + ex.getClass());
+        } catch (Exception ex) {
+            System.out.println("Qualquadra non cosa :-( " + ex.getMessage() + " " + ex.getClass());
+        }
+
         PrintWriter wr = resp.getWriter();
-        wr.println("<h1> Hello from servlet! :-)</h1>");
+        //wr.println("<h1> Hello from servlet! :-)</h1>");
+        wr.println("[\n"
+                + "    {\n"
+                + "        \"cognome\": \"Rossi\",\n"
+                + "        \"nome\": \"Luigi\",\n"
+                + "        \"email\": \"l.rossi@email.it\",\n"
+                + "        \"citta\": \"torino\"\n"
+                + "    }\n]");
     }
 }
